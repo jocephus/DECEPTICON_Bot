f6ad398b33a1930b6baee3f362cfd0f977fdbf75
Joe Gray <jgray@advancedpersistentsecurity.net>
2020-04-20 13:36:21 -0400

    Update decepticon_imports.py

--- decepticon_imports.py
+++ decepticon_imports.py
@@ -8,29 +8,17 @@ import sys
 import re
 import time
 import random
+import heapq
+import pickle
 #Data science imports
 import pandas as pd
 import numpy as np
-#import h5py
-#import tensorflow as tf
-#import keras
-#import collections
-#from tensorflow.keras import layers
-#from keras.models import Sequential
-#from keras.layers import Dense, LSTM, Dropout, Bidirectional, Embedding
-#from keras.callbacks import ModelCheckpoint
 #NLP Imports
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-from textblob import Word
-#Bag of Words Support
-#import scipy as scy
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
+from nltk.corpus import stopwords, PlaintextCorpusReader
+from nltk.parse.generate import generate
+from nltk import ConditionalFreqDist
 #Module specific imports
 import twitter as tw
 import t_keys as keys
