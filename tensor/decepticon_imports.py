292a07bd22513eb33ded109cf2e942cdfe6c8cac
Joe Gray <jgray@advancedpersistentsecurity.net>
2020-07-16 22:32:25 -0400

    Update decepticon_imports.py

--- tensor/decepticon_imports.py
+++ tensor/decepticon_imports.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+  
 
 #Housekeeping imports
 import os
@@ -13,18 +13,19 @@ import pandas as pd
 import numpy as np
 #AIML imports
 import tensorflow as tf
+from keras import backend as K
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
+from keras.optimizers import RMSprop
+from keras.layers import Dense, Dropout, LSTM, Input
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import text_to_word_sequence
-#Spacy
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
 #NLP Imports
 import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 #Module specific imports
 import twitter as tw
 import t_keys as keys
