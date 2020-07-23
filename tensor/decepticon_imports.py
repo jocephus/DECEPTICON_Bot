6e54572c51053849635e6ea1f7b0866c8819bef6
Joe Gray <jgray@advancedpersistentsecurity.net>
2020-07-22 21:59:13 -0400

    Update decepticon_imports.py

--- tensor/decepticon_imports.py
+++ tensor/decepticon_imports.py
@@ -8,6 +8,7 @@ import sys
 import re
 import time
 import random
+import json
 #Data science imports
 import pandas as pd
 import numpy as np
@@ -16,12 +17,14 @@ import tensorflow as tf
 from keras import backend as K
 from keras.models import Sequential
 from keras.optimizers import RMSprop
-from keras.layers import Dense, Dropout, LSTM, Input
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Embedding, Dropout, LSTM, Input
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import text_to_word_sequence
+from keras.models import model_from_json
+import h5py
 #NLP Imports
 import nltk
 from nltk.corpus import stopwords
