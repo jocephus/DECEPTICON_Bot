c19864826916cbd759124ae4ca7fbacdcbacef7c
Joe Gray <jgray@advancedpersistentsecurity.net>
2020-07-22 22:02:37 -0400

    Update decepticon_bot.py

--- tensor/decepticon_bot.py
+++ tensor/decepticon_bot.py
@@ -9,7 +9,8 @@ posts to Twitter for enhanced OPSEC.
 from decepticon_imports import *
 #
 #
-# Thanks to Adrian, Goldar, Dreadjak, Bill, thatguy, nosirrahSec, & jferg for the help.
+# Thanks to Adrian, Goldar, Dreadjak, James, Ch33r10, Rae, nvember, 
+# Bill, thatguy, nosirrahSec, & jferg for the help.
 #
 #
 
@@ -45,14 +46,13 @@ def tokenization(api, USER_DF):
     Tokenization is where the tweets get broken down by word.
     The Bag of Hashtags and Mentions are also generated here as well.
     """
-    results = api.GetUserTimeline(include_rts=False, count=200, exclude_replies=True)
     global BOH
     BOH = []
     global BOM
     BOM = []
     global BOL
     BOL = []
-    for tweet in results:
+    for tweet in RESULTS:
         full_text = tweet.full_text
         full_split = str(full_text.split(' , '))
         hash_regex = re.compile(r"\s(?P<hashtag>\#\S+)\s")
@@ -74,17 +74,18 @@ def hopper(api, USER_DF):
     """
     This module writes to the Dataframe and begins the sorting process.
     """
-    results = api.GetUserTimeline(include_rts=False, count=200, exclude_replies=True)
-    for tweet in results:
+    for tweet in RESULTS:
         full_text = tweet.full_text
         tweet_time = tweet.created_at #Getting the UTC time
         make_time = time.mktime(time.strptime(tweet_time, "%a %b %d %H:%M:%S %z %Y"))
         epoch_time = int(make_time)
         lex_div = lexical_diversity(str(tweet))
-        USER_DF = USER_DF.append({'Tweets': full_text, 'Times': epoch_time, 'LD': lex_div,}, ignore_index=True, sort=True)
-    sorter(full_text, USER_DF)
+        w = re.sub(r'\#\S+', '', str(full_text))
+        ww = re.sub(r'\@\S+', '', str(w))
+        USER_DF = USER_DF.append({'Tweets': ww, 'Times': epoch_time, 'LD': lex_div,}, ignore_index=True, sort=True)
     USER_DF = USER_DF.drop_duplicates(subset=['Times'])
-
+    sorter(full_text, USER_DF)
+    
 
 def sorter(full_text, USER_DF):
     """
@@ -100,18 +101,11 @@ def sorter(full_text, USER_DF):
         w = r['Tweets']
         INTER.append(w)
     t = Tokenizer()
-    t.fit_on_texts(INTER)
+    t.fit_on_texts(str(INTER))
     tokenized_tweets = word_tokenize(str(INTER))
     for token in tokenized_tweets:
-        #if token not in STOP_WORDS:
-            #BOW.append(token)
         BOW.append(token)
     USER_DF.to_csv('files.csv')
-    for i in range(len(INTER)):
-        INTER[i] = re.sub(r'\@\W+', '', str(INTER[i])).split(' , ')
-        INTER[i] = re.sub(r'\s\S+t\.co\/\S+\s', '', str(INTER[i])).split(' , ')
-        INTER[i] = re.sub(r'\/+', '', str(INTER[i])).split(' , ')
-        INTER[i] = re.sub(r'\\+', '', str(INTER[i])).split(' , ')
     stats(USER_DF)
     freq_analysis()
     generate(BOW)
@@ -123,18 +117,18 @@ def stats(USER_DF):
     This prints stats about posting and Lexical diversity
     """
     print(f"\n\nStats for tweets:\n\n")
+    print(f"Lexical Diversity will be used in future versions\n")
     lex_div2 = lexical_diversity(USER_DF['Tweets'])
-    print(f"\nThe Lexical Diversity is:\t\t\t{lex_div2}")
+    print(f"The Lexical Diversity is:\t\t\t{lex_div2}")
     lex_div3 = np.mean(USER_DF['LD'])
-    print(f"\nThe Statistical Mean Lexical Diversity is:\t{lex_div3}")
+    print(f"The Statistical Mean Lexical Diversity is:\t{lex_div3}")
     lex_div4 = np.std(USER_DF['LD'])
-    print(f"\nThe StdDev of Lexical Diversity is:\t\t{lex_div4}")
+    print(f"The StdDev of Lexical Diversity is:\t\t{lex_div4}")
     time_stddev = np.std(USER_DF['Times'])
-    print(f"\n\nTweets occur at this interval:\t\n")
+    print(f"\nTweets occur at this interval:\t\n")
     global POST_INTERVAL
     POST_INTERVAL = int(time_stddev)
-    print(f"\n\t{POST_INTERVAL} seconds apart.\n\n")
-    print('\n')
+    print(f"\t\t{POST_INTERVAL} seconds apart.\n")
 
 
 def freq_analysis():
@@ -143,13 +137,14 @@ def freq_analysis():
     """
     t = Tokenizer()
     t.fit_on_texts(BOH)
-    print(f'Frequency analysis of Hashtags: {t.word_counts}')
+    print('Analysis of trends for future iterations:\n')
+    print(f'Frequency analysis of Hashtags: \n{t.word_counts}\n')
     t = Tokenizer()
     t.fit_on_texts(BOL)
-    print(f'Frequency analysis of Links: {t.word_counts}')
+    print(f'Frequency analysis of Links: \n{t.word_counts}\n')
     t = Tokenizer()
     t.fit_on_texts(BOM)
-    print(f'Frequency analysis of Mentions: {t.word_counts}')
+    print(f'Frequency analysis of Mentions: \n{t.word_counts}\n')
 
 
 def generate(BOW):
@@ -174,7 +169,6 @@ def generate(BOW):
         x_data.append([char_to_num[char] for char in in_seq])
         y_data.append(char_to_num[out_seq])
     n_patterns = len(x_data)
-    #print(f'Total Patterns: {n_patterns}')
     X = np.reshape(x_data, (n_patterns, seq_length, 1))
     X = X/float(VOCAB_LEN)
     y = np_utils.to_categorical(y_data)
@@ -182,39 +176,84 @@ def generate(BOW):
     trainer(x_data, CHARS)
 
 
+def regenerate(BOW):
+    """
+    This kicks off the process of generating text.
+    """
+
+    print('Beginning In-Depth Analysis...\n')
+    global CHARS
+    CHARS = BOW
+    char_to_num = dict((c, i) for i, c in enumerate(CHARS))
+    input_len = len(BOW)
+    global VOCAB_LEN
+    VOCAB_LEN = len(CHARS)
+    print(f'Total number of characters: {input_len}')
+    print(f'Total vocab: {VOCAB_LEN}\n')
+    seq_length = 500
+    x_data = []
+    y_data = []
+    for i in range(0, input_len - seq_length, 1):
+        in_seq = BOW[i:i + seq_length]
+        out_seq = BOW[i + seq_length]
+        x_data.append([char_to_num[char] for char in in_seq])
+        y_data.append(char_to_num[out_seq])
+    n_patterns = len(x_data)
+    X = np.reshape(x_data, (n_patterns, seq_length, 1))
+    X = X/float(VOCAB_LEN)
+    y = np_utils.to_categorical(y_data)
+    premodeling(X, y)
+    trainer(x_data, CHARS)
+    
 def premodeling(X, y):
     """
     This creates the Statistical model for generating Tweets.
     """
     print('Creating the statistical model...\n\n')
-    global MODEL
-    MODEL = Sequential()
-    filename = 'model_weights_saved.hdf5'
+    filename = 'model.h5'
     global FILENAME
     FILENAME = filename
-    if path.exists(filename):
-        MODEL.load_weights(filename)
-        modeling(X, y)
+    if path.exists('model.json'):
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights('model.h5')
+        loaded_model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+        filepath = 'model.h5'
+        checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, \
+        save_best_only=True, mode='min')
+        with tf.device('/gpu:0'):
+            MODEL.fit(X, y, epochs=5, batch_size=32, callbacks=checkpoint)
+            MODEL.load_weights(FILENAME)
+            MODEL.compile(loss='binary_crossentropy', optimizer='rmsprop')
+        model_json = MODEL.to_json()
+        with open('model.json', 'w') as json_file:
+            json_file.write(model_json)
     else:
         modeling(X, y)
 
 def modeling(X, y):
-    MODEL.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+    MODEL.add(LSTM(224, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
     MODEL.add(Dropout(0.6))
-    MODEL.add(LSTM(256, return_sequences=True))
+    MODEL.add(LSTM(224, return_sequences=True))
     MODEL.add(Dropout(0.6))
-    MODEL.add(LSTM(256))
-    MODEL.add(Dropout(0.6))
+    MODEL.add(LSTM(224))
+    MODEL.add(Dropout(0.4))
     MODEL.add(Dense(y.shape[1], activation='softmax'))
     optimizer = RMSprop(lr=0.001)
-    MODEL.compile(loss='categorical_crossentropy', optimizer='adam')
-    filepath = 'model_weights_saved.hdf5'
+    MODEL.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    filepath = 'model.h5'
     checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, \
     save_best_only=True, mode='min')
     with tf.device('/gpu:0'):
-        MODEL.fit(X, y, epochs=3, batch_size=16, callbacks=checkpoint)
+        MODEL.fit(X, y, epochs=5, batch_size=32, callbacks=checkpoint)
         MODEL.load_weights(FILENAME)
-        MODEL.compile(loss='categorical_crossentropy', optimizer='adam')
+        MODEL.compile(loss='binary_crossentropy', optimizer='rmsprop')
+        #MODEL.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model_json = MODEL.to_json()
+    with open('model.json', 'w') as json_file:
+        json_file.write(model_json)
 
 
 def trainer(x_data, CHARS):
@@ -227,7 +266,6 @@ def trainer(x_data, CHARS):
     start = np.random.randint(0, len(x_data) -1)
     pattern = x_data[start]
     print('Random Seed: Created\n\t')
-    #print("\"", ''.join([NUM_TO_CHAR[value] for value in pattern]), "\"\n\n")
     tweet_creator(pattern)
 
 
@@ -247,15 +285,14 @@ def tweet_creator(pattern):
         seq_in = [int_to_char[value] for value in pattern]
         result = ''.join([str(itemInResult) for itemInResult in result])
         result = result.split(' , ')
-        #print(f'The result is:\n\n\t{result}\n')
         pattern.append(index)
         pattern = pattern[1:len(pattern)]
-    logic_check(pattern)
+    cleanup(pattern)
 
 
-def logic_check(pattern):
+def cleanup(pattern):
     """
-    This module merely checks some logic and adds some con-specific strings
+    This module cleans up the proposed tweet
     """
     logic_one = pattern[:40]
     logic_two = str([NUM_TO_CHAR[value] for value in logic_one])
@@ -265,8 +302,8 @@ def logic_check(pattern):
     logic_two = re.sub(r"\/\/t\.co\/\S+", "", str(logic_two))
     logic_two = re.sub(r"\[", "", str(logic_two))
     logic_two = re.sub(r"\]", "", str(logic_two))
-    logic_two = re.sub(r"\@\s", "@", str(logic_two))
-    logic_two = re.sub(r"\#\s", "#", str(logic_two))
+    logic_two = re.sub(r"\@\S+", "", str(logic_two))
+    logic_two = re.sub(r"\#\S+", "", str(logic_two))
     logic_two = re.sub(r"\\n", "", str(logic_two))
     logic_two = re.sub(r"\"", "", str(logic_two))
     logic_two = re.sub(r"\\", "", str(logic_two))
@@ -275,48 +312,22 @@ def logic_check(pattern):
     logic_two = re.sub(r"\)", "", str(logic_two))
     logic_two = re.sub(r"\s\:", ":", str(logic_two))
     logic_two = re.sub(r"https", "", str(logic_two))
-    con = np.random.randint(0,7)
-    if con == 0:
-        api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        repeater()
-    elif con == 1:
-        api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @sectorca'))
-        print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        repeater()
-    elif con == 2:
-        api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @hackinthebox'))
-        print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        repeater()
-    elif con == 3:
-        api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @hackfest_ca'))
-        print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        repeater()
-    elif con == 4:
-        api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @hackfest_ca'))
-        print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        repeater()
-    elif con == 5:
-        api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @NoNameConOrg'))
-        print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        repeater()
-    elif con == 6:
-        api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @hopeconf'))
-        print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        repeater()
-    else:
-        api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @x33fcon'))
-        print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + '#DECEPTICON @BlackHatEvents'))
-        repeater()
+    logic_two = re.sub(r"\'", "", str(logic_two))
+    logic_two = re.sub(r"\"", "", str(logic_two))
+    logic_two = re.sub(r"\,", "", str(logic_two))
+    logic_two = re.sub(r"\`", "", str(logic_two))
+    logic_check(logic_two)
+    api.PostUpdate(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + ' #DECEPTICON'))
+    print(str(''.join([str(itemInLogic_two) for itemInLogic_two in logic_two]) + ' #DECEPTICON'))
+    repeater()
 
 
 def repeater():
     """
     If a continuation is requested, this will trigger repetition.
     """
-    os.remove(FILENAME)
     #sleeping_interval = POST_INTERVAL-(random.randint(0, 480))
-    sleeping_interval = (random.randint(14400, 28800))
+    sleeping_interval = (random.randint(1440, 9800))
     print(f"\t\t\tSleeping for {sleeping_interval} seconds...\t\t\t")
     sleeping_hours = (sleeping_interval / 3600)
     print(f"\t\t\tSleeping for {sleeping_hours} hours...\t\t\t")
@@ -328,14 +339,13 @@ def subsequent():
     """
     This initializes further repetition.
     """
-    results = api.GetUserTimeline(include_rts=False, count=200, exclude_replies=True)
     global BOH
     BOH = []
     global BOM
     BOM = []
     global BOL
     BOL = []
-    for tweet in results:
+    for tweet in RESULTS:
         full_text = tweet.full_text
         full_split = str(full_text.split(' , '))
         hash_regex = re.compile(r"\s(?P<hashtag>\#\S+)\s")
@@ -372,5 +382,7 @@ api = tw.Api(consumer_key=keys.consumer_key,
              consumer_secret=keys.consumer_secret,
              access_token_key=keys.access_token_key,
              access_token_secret=keys.access_token_secret,
-             tweet_mode='extended')
+             tweet_mode='extended', sleep_on_rate_limit=True)
+MODEL = Sequential()
+RESULTS = api.GetUserTimeline(include_rts=False, count=200, exclude_replies=True)
 login()
