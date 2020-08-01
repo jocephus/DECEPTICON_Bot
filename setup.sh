8e32ea1d3aea3ad25655820d72d28454ca117bb1
Joe Gray <jgray@advancedpersistentsecurity.net>
2020-08-01 14:10:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-apt-get update -y; apt-get upgrade -y; apt-get dist-upgrae -y; apt automremove -y;
-apt-get install python3 python3-pip git;
+apt-get update -y; apt-get upgrade -y; apt-get dist-upgrade -y; apt autoremove -y;
+apt-get install python3 python3-pip git python3-venv;
 python3 -m pip install --no-use-wheel --no-cache-dir Cython;
 python3 -m pip install -U -r requirements.txt;
